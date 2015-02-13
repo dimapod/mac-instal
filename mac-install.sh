c3f4ac60cfcc557c397af19586a30dbcc2ef32cb
dimapod <dimapod@gmail.com>
2015-02-13 10:49:17 +0100

    Install scripts

---
+++ mac-install.sh
@@ -0,0 +1,31 @@
+
+
+brew doctor
+
+brew install wget
+
+brew install caskroom/cask/brew-cask
+
+brew cask install google-chrome
+
+brew cask install virtualbox
+
+brew cask install iterm2
+
+brew cask install intellij-idea
+
+brew cask install spotify
+
+brew cask install vlc
+
+brew cask install java
+
+brew cask install visualvm
+
+brew cask install kodi
+
+brew cask install evernote
+
+brew cask install virtualbox
+
+brew cask install android-file-transfer
